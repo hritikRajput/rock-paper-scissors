af6d31c193db6af14e68e90225f1682a960c146d
hritikRajput <hritikshivam@gmail.com>
2022-12-30 21:23:24 +0530

    Add index.html, css directory, and app.js file

---
+++ app.js
@@ -0,0 +1,21 @@
+// rock beat scissor
+//scissor beat paper
+//paper beat rock
+
+/* 
+  if computer play rock:
+  --if you play paper then you won
+  --if you play scissor then you lose
+  -- otherwise game equal
+
+  if computer play scissor:
+  --if you play rock then you won
+  --if you play paper then you lose
+  -- otherwise game equal
+
+  if computer play paper:
+  --if you play scissor then you won
+  --if you play rock then you lose
+  -- otherwise game equal
+
+*/
