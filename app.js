bd35f9c42cdfecd14fb4e86568896127732ce7a0
hritikRajput <hritikshivam@gmail.com>
2023-01-03 18:30:39 +0530

    Add html code and style to CSS, Refactor app.js

--- app.js
+++ app.js
@@ -19,6 +19,8 @@
   -- otherwise game equal
 
 */
+let playerPoints = 0;
+let computerPoints = 0;
 
 function getComputerChoice() {
     let choices = ["rock", "paper", "scissor"]
@@ -28,60 +30,68 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return (["Game draw",0,0])
+    if (playerSelection.toLowerCase() === computerSelection) {
+        return "Game draw"
     }
 
     if (playerSelection.toLowerCase() === "rock") {
         if (computerSelection === "paper") {
-            return (["You Lose! Paper beats Rock",0,1])
+            computerPoints++;
+            return "You Lose! Paper beats Rock"
         }
         else {
-            return (["You Wonnnnnn! Rock beats Scissor",1,0])
+            playerPoints++;
+            return "You Wonnnnnn! Rock beats Scissor"
         }
     }
 
     if (playerSelection.toLowerCase() === "paper") {
         if (computerSelection === "scissor") {
-            return (["You Lose! Scissor beats paper",0,1])
+            computerPoints++;
+            return "You Lose! Scissor beats paper"
         }
         else {
-            return (["You Wonnnnnn! Paper beats Rock",1,0])
+            playerPoints++;
+            return "You Wonnnnnn! Paper beats Rock"
         }
     }
     if (playerSelection.toLowerCase() === "scissor") {
         if (computerSelection === "rock") {
-            return (["You Lose! Rock beats Scissor",0,1])
+            computerPoints++;
+            return "You Lose! Rock beats Scissor"
         }
         else {
-            return (["You Wonnnnnn! Scissor beats Paper",1,0])
+            playerPoints++;
+            return "You Wonnnnnn! Scissor beats Paper"
         }
     }
 }
 
 
-function game() {
-    let playerPoints = 0;
-    let computerPoints = 0;
-    for (let i = 1; i <= 5; i++) {
-        const playerSelection = prompt("What is your choice Rock, Paper or Scissor? ");
-        const computerSelection = getComputerChoice()
-        let resultData = playRound(playerSelection, computerSelection)
-        playerPoints+=resultData[1]
-        computerPoints+=resultData[2]
-        console.log(resultData[0])
+function game(e) {
+    const playerSelection = e.target.innerText;
+    const computerSelection = getComputerChoice()
+    let resultData = playRound(playerSelection, computerSelection)
+    resultString = `${resultData}\nPlayer Points: ${playerPoints}     Computer Points: ${computerPoints}`
+
+    if (playerPoints === 5) {
+        resultString = resultString + "\n You Won! Game Over"
+        playerPoints = 0;
+        computerPoints = 0;
+
     }
-    console.log(`After 5 round your score is ${playerPoints} and computer score is ${computerPoints}.`)
-    if(playerPoints===computerPoints){
-        console.log("Game draw. Nobody lose.")
+    else if (computerPoints === 5) {
+        resultString = resultString + "\n You lose! Game Over"
+        playerPoints = 0;
+        computerPoints = 0;
     }
-    else if(playerPoints>computerPoints){
-        console.log("You won. Partyyyyyyyyy")
-    }
-    else{
-        console.log("You lose! Better luck next time")
-    }
+
+    result.innerText = resultString;
 }
 
 
-game()
+const buttons = document.querySelectorAll('button');
+const result = document.querySelector('.result');
+buttons.forEach(button => {
+    button.addEventListener('click', game);
+});
